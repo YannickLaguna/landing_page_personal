d33122161b8fe6482eed0729fb151e1dd2daef6a
YannickLaguna <yannickylp0203@gmail.com>
2025-07-07 13:50:31 +0200

    joooodeeeeerrrrr

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,12 @@
 import { Config, ContentObject, ThemeStyle } from './generated';
 
 export * from './base';
-export * from './generated';
+
+export type {
+    Button, Config, ContentObject, ContentObjectMetadata, ContentObjectType, FeaturedPostsSection, FeaturedProjectsSection, Footer, Header, ImageBlock, Link, PageLayout, Person, PostFeedSection, PostLayout, ProjectFeedSection, ProjectLayout, RecentPostsSection, RecentProjectsSection, SectionModels, Testimonial,
+    TestimonialsSection,
+    TextSection, ThemeStyle
+} from './generated';
 
 export type GlobalProps = {
     site: Config;
